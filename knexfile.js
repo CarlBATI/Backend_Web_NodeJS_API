7a625bd739f0fc3f5f0c422fac0ee41f5a1386d4
carl-a-van-roosbroeck <carl.a.van.roosbroeck@protonmail.me>
2024-01-13 23:20:38 +0100

    Add Knex.js and MySQL dependencies, create knexfile.js, update BACKLOG.org

---
+++ knexfile.js
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default {
+
+  development:
+
+ {
+    client: 'mysql',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  // Add other environments (staging, production) as needed
+};
