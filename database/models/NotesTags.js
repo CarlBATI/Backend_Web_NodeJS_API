fb573c2d7c1977205cc1bc78928807bf3b5a06fa
carl-a-van-roosbroeck <carl.a.van.roosbroeck@protonmail.me>
2024-01-15 00:58:29 +0100

    Create models/ dir for simple Models, consts like X_TABLE_NAME, X_FIELD_MAX_LENGTH, etc.

---
+++ database/models/NotesTags.js
@@ -0,0 +1,5 @@
+const NOTES_TAGS_TABLE_NAME = 'Notes_Tags';
+
+module.exports = {
+    NOTES_TAGS_TABLE_NAME
+};
---
+++ database/models/Tags.js
@@ -0,0 +1,10 @@
+const TAGS_TABLE_NAME = 'Tags';
+
+const TAG_NAME_MAX_LENGTH = 25;
+const TAG_NAME_MIN_LENGTH = 1;
+
+module.exports = {
+    TAGS_TABLE_NAME,
+    TAG_NAME_MAX_LENGTH,
+    TAG_NAME_MIN_LENGTH
+};
