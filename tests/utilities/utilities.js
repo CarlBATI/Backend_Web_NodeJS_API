3060d0d9f33247a61c2405de2f317eddb998c15d
carl-a-van-roosbroeck <carl.a.van.roosbroeck@protonmail.me>
2024-01-15 12:18:41 +0100

    Add testGetSingleObjectById function to utilities.js

--- tests/utilities/utilities.js
+++ tests/utilities/utilities.js
@@ -39,7 +39,8 @@ async function testPostObjectCreation(path, object, properties, verbose = false)
 }
 
 /**
- * Test bad request
+ * Test bad request - POST
+ * Sending a malformed object should return status 400 Bad Request
  * 
  * @param {string} path - The path to send the request to
  * @param {object} object - The object to send in the request body. Must have all required properties.
@@ -62,7 +63,41 @@ async function testPostBadRequest(path, object, verbose = false) {
     }
 }
 
+/**
+ * Test reading a single object - GET
+ * Objects must have an id property
+ * A database object must exist with the same id as the object in the request
+ * The expected object must have an id property with a value that matches the id of the object in the database
+ * 
+ * @param {string} path - The path to send the request to
+ * @param {object} expectedObject - The object to compare the response body to. 
+ * @returns {Promise} - A promise that resolves to the response from the server
+ * @throws {Error} - An error if the response status code is not 200
+ * @throws {Error} - An error if the response body does not have an id property
+ * @throws {Error} - An error if the response body does not have the correct properties
+ * 
+ * @example
+ * const path = '/tags/1';
+ * const expectedObject = { id: 1, name: 'Test Tag' };
+ * await testGetSingleObject(path, expectedObject);
+ */
+async function testGetSingleObjectById(path, expectedObject, verbose = false) {
+    const response = await request(server).get(path);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveProperty('id');
+    expect(response.body.id).toBe(Number(expectedObject.id));
+
+    // iterate over the properties of the expected object and compare them to the response body
+    for (const property in expectedObject) {
+        expect(response.body[property]).toBe(expectedObject[property]);
+    }
+
+    if (verbose) console.log(`GET ${path} test for ID: ${response.body.id}\n${JSON.stringify(response.body, null, 2)}`);
+}
+
 module.exports = {
     testPostObjectCreation,
-    testPostBadRequest
+    testPostBadRequest,
+    testGetSingleObjectById
 };
