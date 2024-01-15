9ebb6aa300066e87885b2cbcf7c22847daf1e1c7
carl-a-van-roosbroeck <carl.a.van.roosbroeck@protonmail.me>
2024-01-15 01:02:40 +0100

    Add documentation and refactor clearDatabase function to delete all records from the database. Has verbose option for allowing console.logs

--- database/clearDatabase.js
+++ database/clearDatabase.js
@@ -1,15 +1,34 @@
+// Cleanup script to clear the database. Meaning, it deletes all the records from the database.
+// Can be run directly from the command line, or imported and used in other scripts.
+// ========================================================
+
+// Imports
 const { query, closePool } = require('./connection');
+const { NOTE_TABLE_NAME } = require('../database/models/Notes')
+const { TAGS_TABLE_NAME } = require('../database/models/Tags')
+const { NOTES_TAGS_TABLE_NAME } = require('../database/models/NotesTags')
 
-async function clearDatabase() {
-    // List of tables to clear
-    // Must be in the right order to avoid foreign key constraint errors
-    const tables = ['Notes'];
+/**
+ * Clears the database
+ * 
+ * @param {boolean} verbose If true, logs more information to the console
+ * @returns {Promise<void>} A promise that resolves when the database is cleared
+ */
+async function clearDatabase(verbose = false) {
+    // The order of the tables is important because of foreign key constraints
+    const tables = [NOTES_TAGS_TABLE_NAME, NOTE_TABLE_NAME, TAGS_TABLE_NAME];
 
-    for (let table of tables) {
-        console.log(`Clearing table ${table}...`);
-        await query(`DELETE FROM ${table}`);
+    try {
+        // Delete all records from each table
+        await Promise.all(tables.map(table => {
+            if (verbose) console.log(`Clearing table ${table}...`);
+            return query(`DELETE FROM ${table}`);
+        }));
+        if (verbose) console.log('Database cleared');
+    } catch (err) {
+        if (verbose) console.error('Error clearing database:', err);
+        throw err; // re-throw the error so it can be caught and handled by the calling code
     }
-    console.log('Database cleared');
 }
 
 // If this file is run directly, clear the database
@@ -18,9 +37,10 @@ if (require.main === module) {
     clearDatabase()
     .catch(console.error)
     .finally(() => {
+        // Close the database connection pool after the function completes
         closePool();
     });
-}
+};
 
 // Otherwise, export the function
 module.exports = clearDatabase;
